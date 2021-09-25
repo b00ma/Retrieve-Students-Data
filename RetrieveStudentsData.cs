51dd3081a48700bdeebb4953f088b5b0a41646b3
A Poornima <89335418+b00ma@users.noreply.github.com>
2021-09-25 17:59:14 +0530

    Add files via upload

---
+++ RetrieveStudentsData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RetrieveStudentsData
+{
+    class RetrieveStudentsData
+    {
+        static void Main()
+        {
+            string text = System.IO.File.ReadAllText(@"E:\VS_C#_Programs\Phase1_Assignments\Retrieve_Students_Data\StudentsData.txt");
+
+            // To display the file contents 
+            System.Console.WriteLine("\nContents of Students Data Text File:\n{0}", text);
+            System.Console.ReadKey();
+        }
+    }
+}
